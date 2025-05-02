ef2ad98c0bff4ac7b9558f7db50d43c96b844445
Mark Woodard <mark.c.woodard@gmail.com>
2025-05-02 09:27:15 -0500

    removing font-family from the mappings section

--- src/kbs-blocks/style-book/component-mapping-control/constants.js
+++ src/kbs-blocks/style-book/component-mapping-control/constants.js
@@ -1,10 +1,10 @@
 import { __ } from '@wordpress/i18n';
 
 export const MAPPING_COMPONENT_OPTIONS = {
-	'font-family': {
-		label: __('Font Family', 'kadence-blocks'),
-		options: ['body', 'heading', 'heading-1', 'heading-2', 'heading-3', 'heading-4', 'heading-5', 'heading-6'],
-	},
+	// 'font-family': {
+	// 	label: __('Font Family', 'kadence-blocks'),
+	// 	options: ['body', 'heading', 'heading-1', 'heading-2', 'heading-3', 'heading-4', 'heading-5', 'heading-6'],
+	// },
 	'font-size': {
 		label: __('Font Size', 'kadence-blocks'),
 		options: ['sm', 'md', 'lg', 'xl', 'xxl', '3xl'],
