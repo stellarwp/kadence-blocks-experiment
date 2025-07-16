12575e08647dd52821ee5aef0ea3e62610d110e6
Mark Woodard <mark.c.woodard@gmail.com>
2025-07-16 16:43:34 -0500

    changing some props in genIcon that were throwing errors.

--- src/kbs-packages/components/src/icon-control/gen-icon/index.js
+++ src/kbs-packages/components/src/icon-control/gen-icon/index.js
@@ -2,72 +2,66 @@ import PropTypes from 'prop-types';
 
 import { createElement } from '@wordpress/element';
 
-export default function GenIcon( props ) {
-	const {
-		className,
-		icon,
-		name,
-		ariaHidden,
-		title
-	} = props;
+export default function GenIcon(props) {
+	const { className, icon, name, ariaHidden, title } = props;
 
-	const isLineIcon = name.startsWith( 'fe' );
+	const isLineIcon = name.startsWith('fe');
 
 	const svgProps = {
-		'aria-hidden': ariaHidden ? 'true' : undefined,
-		'stroke-linecap': isLineIcon ? 'round' : undefined,
-		'stroke-linejoin': isLineIcon ? 'round' : undefined,
+		ariaHidden: ariaHidden ? 'true' : undefined,
+		strokeLinecap: isLineIcon ? 'round' : undefined,
+		strokeLinejoin: isLineIcon ? 'round' : undefined,
 		display: 'inline-block',
 		verticalAlign: 'middle',
 	};
 
-	const typeL = name.substring( 0, 3 );
-	const viewBoxParts = icon?.vB?.split( ' ' );
-	const hasOffsetViewBox = viewBoxParts && ( viewBoxParts[0] !== '0' || viewBoxParts[1] !== '0' );
+	const typeL = name.substring(0, 3);
+	const viewBoxParts = icon?.vB?.split(' ');
+	const hasOffsetViewBox = viewBoxParts && (viewBoxParts[0] !== '0' || viewBoxParts[1] !== '0');
 
-	if ( !['fas', 'fe_', 'ic_'].includes( typeL ) && hasOffsetViewBox ) {
+	if (!['fas', 'fe_', 'ic_'].includes(typeL) && hasOffsetViewBox) {
 		svgProps.preserveAspectRatio = 'xMinYMin meet';
 	}
 
 	return (
 		<div
-			style={ { display: 'inline-flex', justifyContent: 'center', alignItems: 'center' } }
-			className={ `${className || ''}` }
+			style={{ display: 'inline-flex', justifyContent: 'center', alignItems: 'center' }}
+			className={`${className || ''}`}
 		>
 			<svg
-				style={ svgProps }
-				viewBox={ icon?.vB || '0 0 24 24' }
-				fill={ isLineIcon ? 'none' : 'currentColor' }
-				stroke={ isLineIcon ? 'currentColor' : undefined }
-				xmlns={ 'http://www.w3.org/2000/svg' }
+				style={svgProps}
+				viewBox={icon?.vB || '0 0 24 24'}
+				fill={isLineIcon ? 'none' : 'currentColor'}
+				stroke={isLineIcon ? 'currentColor' : undefined}
+				xmlns={'http://www.w3.org/2000/svg'}
 			>
-				{ title && <title>{ title }</title> }
-				{ icon && walkChildren( icon.cD ) }
+				{title && <title>{title}</title>}
+				{icon && walkChildren(icon.cD)}
 			</svg>
 		</div>
 	);
-};
+}
 
-const walkChildren = ( children ) => {
-	return children.map( ( child, idx ) => {
+const walkChildren = (children) => {
+	return children.map((child, idx) => {
 		const { nE: nodeName, aBs: attributes, children: grandChildren = null } = child;
 
-		const newAttributes = Object.keys( attributes )
-			.filter( key => key !== 'fill' && key !== 'stroke' && attributes[ key ] !== 'none' )
-			.reduce( ( partial, key ) => {
-				partial[ key ] = attributes[ key ];
+		const newAttributes = Object.keys(attributes)
+			.filter((key) => key !== 'fill' && key !== 'stroke' && attributes[key] !== 'none')
+			.reduce((partial, key) => {
+				partial[key] = attributes[key];
 				return partial;
-			}, {} );
+			}, {});
 
 		// Special case for icons that have both fill and stroke properties.
 		let merge = {};
-		if ( attributes.fill === 'none' && attributes.stroke ) {
+		if (attributes.fill === 'none' && attributes.stroke) {
 			merge = { fill: 'none', stroke: 'currentColor' };
 		}
 		return createElement(
 			nodeName,
 			{ key: idx, ...newAttributes, ...merge },
-			grandChildren ? walkChildren( grandChildren ) : null
+			grandChildren ? walkChildren(grandChildren) : null
 		);
-	} );
+	});
 };
