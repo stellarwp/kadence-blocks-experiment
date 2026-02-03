2a3d168941f208d730a8a9dcfc1ea070a6bbd702
Lint Action <lint-action@samuelmeuli.com>
2026-02-03 10:43:41 +0000

    Fix code style issues with ESLint

--- tests/e2e/playwright.config.js
+++ tests/e2e/playwright.config.js
@@ -1,91 +1,91 @@
-const { loadConfig } = require( '@wordpress/env/lib/config' );
-const { defineConfig, devices } = require( '@playwright/test' );
+const { loadConfig } = require('@wordpress/env/lib/config');
+const { defineConfig, devices } = require('@playwright/test');
 
 /**
  * @see https://playwright.dev/docs/test-configuration
  */
-module.exports = ( async () => {
-    // Get Base URL from wp-env.
-    const wpEnvConfig = await loadConfig( './' );
-    let baseUrl = 'http://localhost';
-    if ( wpEnvConfig ) {
-        const port = wpEnvConfig.env.tests.port || null;
+module.exports = (async () => {
+	// Get Base URL from wp-env.
+	const wpEnvConfig = await loadConfig('./');
+	let baseUrl = 'http://localhost';
+	if (wpEnvConfig) {
+		const port = wpEnvConfig.env.tests.port || null;
 
-        if ( port ) {
-            baseUrl = wpEnvConfig.env.tests.config.WP_SITEURL;
-        }
-    }
+		if (port) {
+			baseUrl = wpEnvConfig.env.tests.config.WP_SITEURL;
+		}
+	}
 
-    return defineConfig( {
-        testDir: '../../tests',
+	return defineConfig({
+		testDir: '../../tests',
 
-        /* Maximum time one test can run for. */
-        timeout: 90 * 1000,
+		/* Maximum time one test can run for. */
+		timeout: 90 * 1000,
 
-        expect: {
-            /**
-             * Maximum time expect() should wait for the condition to be met.
-             * For example in `await expect(locator).toHaveText();`
-             */
-            timeout: 20 * 1000,
-        },
+		expect: {
+			/**
+			 * Maximum time expect() should wait for the condition to be met.
+			 * For example in `await expect(locator).toHaveText();`
+			 */
+			timeout: 20 * 1000,
+		},
 
-        /* Folder for test artifacts such as screenshots, videos, traces, etc. */
-        outputDir: './test-results/report',
+		/* Folder for test artifacts such as screenshots, videos, traces, etc. */
+		outputDir: './test-results/report',
 
-        /* Run tests in files in parallel */
-        fullyParallel: false,
+		/* Run tests in files in parallel */
+		fullyParallel: false,
 
-        /* Fail the build on CI if you accidentally left test.only in the source code. */
-        forbidOnly: !! process.env.CI,
+		/* Fail the build on CI if you accidentally left test.only in the source code. */
+		forbidOnly: !!process.env.CI,
 
-        /* Retry on CI only */
-        retries: process.env.CI ? 2 : 0,
+		/* Retry on CI only */
+		retries: process.env.CI ? 2 : 0,
 
-        /* Opt out of parallel tests on CI. */
-        workers: process.env.CI ? 1 : 1,
+		/* Opt out of parallel tests on CI. */
+		workers: process.env.CI ? 1 : 1,
 
-        /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-        reporter: [
-            [ 'list' ],
-            [
-                'html',
-                {
-                    outputFolder: './test-results/playwright-report',
-                    open: 'never',
-                },
-            ],
-        ],
+		/* Reporter to use. See https://playwright.dev/docs/test-reporters */
+		reporter: [
+			['list'],
+			[
+				'html',
+				{
+					outputFolder: './test-results/playwright-report',
+					open: 'never',
+				},
+			],
+		],
 
-        /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-        use: {
-            /* Base URL to use in actions like `await page.goto('/')`. */
-            baseURL: baseUrl,
+		/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+		use: {
+			/* Base URL to use in actions like `await page.goto('/')`. */
+			baseURL: baseUrl,
 
-            /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-            trace: 'retain-on-failure',
+			/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+			trace: 'retain-on-failure',
 
-            // Capture screenshot after each test failure.
-            screenshot: 'only-on-failure',
+			// Capture screenshot after each test failure.
+			screenshot: 'only-on-failure',
 
-            stateDir: './tests/e2e/test-results/storage/',
+			stateDir: './tests/e2e/test-results/storage/',
 
-            // Record video only when retrying a test for the first time.
-            video: 'on-first-retry',
+			// Record video only when retrying a test for the first time.
+			video: 'on-first-retry',
 
-            // Viewport used for all pages in the context.
-            viewport: { width: 1280, height: 720 },
-        },
+			// Viewport used for all pages in the context.
+			viewport: { width: 1280, height: 720 },
+		},
 
-        /* Configure projects for major browsers */
-        projects: [
-            {
-                name: 'chromium',
-                use: { ...devices[ 'Desktop Chrome' ] },
-            },
-        ],
+		/* Configure projects for major browsers */
+		projects: [
+			{
+				name: 'chromium',
+				use: { ...devices['Desktop Chrome'] },
+			},
+		],
 
-        // path to the global setup files.
-        globalSetup: require.resolve( './global-setup' ),
-    } );
-} )();
+		// path to the global setup files.
+		globalSetup: require.resolve('./global-setup'),
+	});
+})();
